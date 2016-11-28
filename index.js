59c295460d625f09e3454176d135be6bed40b6c0
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-11-27 23:41:03 -0800

    Adding console-ui

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const App = require('./lib/app');
 const CLI = require('./lib/cli');
 const UI = require('./lib/ui');
 
-const ui = new UI();
+const ui = new UI({
+  inputStream: process.stdin,
+  outputStream: process.stdout
+});
+
 const app = new App(ui, pkg.version, process.env.NODE_ENV);
 const cli = new CLI(app, ui);
 
